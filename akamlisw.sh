996560551443143702b8b656cdbf70ad8b931dc0
Ayyoub Kamli <akamli@e1r6p13.1337.ma>
2022-02-09 21:45:09 +0100

    add port to the url

--- akamlisw.sh
+++ akamlisw.sh
@@ -2,7 +2,7 @@ sudo -i
 yum update
 yum install curl
 export K3S_TOKEN_FILE=/vagrant_shared/token
-export K3S_URL=https://192.168.42.110
+export K3S_URL=https://192.168.42.110:6443
 export INSTALL_K3S_EXEC="--flannel-iface=eth1"
 curl -sfL https://get.k3s.io | sh
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
