9994b2c7ab7d8e1d5a1c5548a868ee471a2f057b
Ayyoub Kamli <akamli@e2r8p11.1337.ma>
2022-02-12 18:37:50 +0100

    party 2 solveed with centos

--- p2/akamlis.sh
+++ p2/akamlis.sh
@@ -1,22 +1,23 @@
 
-yum update
+yum update -y
 yum install curl
 firewall-cmd --permanent --add-port=6443/tcp
 firewall-cmd --permanent --add-port=6443/udp
+firewall-cmd --permanent --add-port=80/tcp
+firewall-cmd --permanent --add-port=80/udp
+firewall-cmd --permanent --add-port=8080/tcp
+firewall-cmd --permanent --add-port=8080/udp
 firewall-cmd --reload
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--flannel-iface eth1" K3S_KUBECONFIG_MODE="644" sh -
-sleep 20
-
+echo "execute configuration"
 while true
     do
-        kubectl get nodes | grep -q 'NotReady'
+        /usr/local/bin/kubectl apply -f /vagrant_shared/conf/
         if [ $? == 0 ]; then
-            echo "NOOOOT RDY"
-        else
-            echo "DEPLOYING APPS"
-            kubectl apply -f /vagrant_shared/conf
+            echo "Configuration executed with success"
             break
+        else
+            echo "kubectl failed. wait for retry... "
+            sleep 5
         fi
-        sleep 1
     done
-    SHELL
