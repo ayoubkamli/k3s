21db64ee08773b1c106b899e1c8ea3cc9958d288
Ayyoub Kamli <akamli@e2r8p11.1337.ma>
2022-02-15 23:00:36 +0100

    change mode +x

--- bonus/scripts/gitlab_install.sh
+++ bonus/scripts/gitlab_install.sh
@@ -10,13 +10,14 @@ NC='\033[0m' # No Color
 #------------------------ install gitlab locally -------------------------------
 sudo curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.deb.sh | sudo bash
 sudo EXTERNAL_URL="http://$IP" apt-get install gitlab-ee
+sudo printf "prometheus_monitoring['enable'] = false\ngrafana['enable'] = false\n" >> /etc/gitlab/gitlab.rb
 sudo gitlab-ctl reconfigure
-echo "Username: root" > /home/${SUDO_USER}/git-credentiels.txt && \
+printf "Username: root\n" > /home/${SUDO_USER}/git-credentiels.txt && \
 sudo cat /etc/gitlab/initial_root_password | grep -v "#" | grep "\S" >> /home/${SUDO_USER}/git-credentiels.txt
-echo "################## go to $IP and coonect using ########################"
+printf "################## go to $IP and coonect using ########################\n"
 sudo cat /home/${SUDO_USER}/git-credentiels.txt
-echo "#######################################################################"
-echo "username and password saved in ~/git-credentiels.txt"
-echo $IP
-echo "END - installing gitlab - "
+printf "#######################################################################\n"
+printf "username and password saved in ~/git-credentiels.txt\n"
+printf $IP
+printf "END - installing gitlab - \n"
 #-------------------------------------------------------------------------------
